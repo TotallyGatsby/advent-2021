c8c8b4bc9b9dc9866c158621aa76134e8772f042
TotallyGatsby <700673+TotallyGatsby@users.noreply.github.com>
2021-12-07 08:47:34 -0800

    Solution to part 2 (Day 7)

--- src/day07/problem1/mod.rs
+++ src/day07/problem1/mod.rs
@@ -1,5 +1,15 @@
+#[allow(dead_code)]
 fn get_fuel_cost(crabs: &Vec<isize>, pos: isize) -> isize {
   let fuel_cost = crabs.iter().fold(0, |cost, crab| cost + (crab - pos).abs());
+  return fuel_cost;
+}
+
+fn get_expensive_fuel_cost(crabs: &Vec<isize>, pos: isize) -> isize {
+  let fuel_cost = crabs.iter().fold(0, |cost, crab| {
+    let diff = (crab - pos).abs();
+    let triangle = (diff * (diff + 1)) / 2;
+    cost + triangle
+  });
   return fuel_cost;
 }
 
@@ -16,15 +26,11 @@ pub fn solve() {
   let mut min = 999999999999;
 
   for i in 0..(max + 1) {
-    let cost = get_fuel_cost(&crabs, i);
+    let cost = get_expensive_fuel_cost(&crabs, i);
     if cost < min {
       min = cost;
     }
   }
-  /*
-  let mut fuel_use
-  [0..*max].binary_search_by(|probe| {
 
-  });*/
   println!("Min Fuel: {}", min);
 }
