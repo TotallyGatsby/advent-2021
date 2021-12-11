49bbe137cdd6e63033e5db90311c04380cb670b4
TotallyGatsby <700673+TotallyGatsby@users.noreply.github.com>
2021-12-11 09:08:50 -0800

    Complete problem 2

--- src/day10/problem1/mod.rs
+++ src/day10/problem1/mod.rs
@@ -6,13 +6,16 @@ pub fn solve() {
     .split('\n')
     .collect::<Vec<_>>();
 
-  let mut corrupt_lines = Vec::new();
+  let mut incomplete_lines = Vec::new();
 
   let mut corruption_score = 0;
+  let mut autocomplete_scores = Vec::new();
 
   for line in lines {
     let mut char_stack = VecDeque::new();
 
+    // Build the stack of opening braces
+    // pop if a matching close brace is found
     let mut is_corrupt = false;
     for char in line.chars() {
       if char == '<' || char == '(' || char == '{' || char == '[' {
@@ -54,11 +57,36 @@ pub fn solve() {
       }
     }
 
-    if is_corrupt {
-      corrupt_lines.push(line);
+    // Autocomplete
+    if !is_corrupt {
+      let mut cur_score: usize = 0;
+      while let Some(bracket) = char_stack.pop_back() {
+        cur_score *= 5;
+        if bracket == '(' {
+          cur_score += 1;
+        } else if bracket == '[' {
+          cur_score += 2;
+        } else if bracket == '{' {
+          cur_score += 3;
+        } else if bracket == '<' {
+          cur_score += 4;
+        }
+      }
+
+      autocomplete_scores.push(cur_score);
+    }
+    if !is_corrupt {
+      incomplete_lines.push(line);
     }
   }
 
+  autocomplete_scores.sort_unstable();
+
   //println!("Corrupt Lines: {:?}", corrupt_lines);
-  println!("Score: {}", corruption_score);
+  println!("Corruption Score: {}", corruption_score);
+  println!("Autocomplete Scores: {:?}", autocomplete_scores);
+  println!(
+    "Central Autocomplete Score: {}",
+    autocomplete_scores[autocomplete_scores.len() / 2]
+  );
 }
