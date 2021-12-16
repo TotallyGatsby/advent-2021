325be5f513090ae9d4d35c4330643c692623ee24
TotallyGatsby <700673+TotallyGatsby@users.noreply.github.com>
2021-12-15 16:15:24 -0800

    Day 15 problem 2

--- src/day15/problem1/mod.rs
+++ src/day15/problem1/mod.rs
@@ -2,12 +2,12 @@ pub mod grid;
 
 #[allow(dead_code)]
 pub fn solve() {
-  let lines = include_str!("sample.txt")
+  let lines = include_str!("input.txt")
     .trim()
     .split('\n')
     .collect::<Vec<_>>();
 
-  let mut sea_grid = grid::SeaGrid::new(lines, 1, 1);
+  let mut sea_grid = grid::SeaGrid::new(lines, 5, 5);
 
   println!("Path Cost: {}", sea_grid.get_route_cost());
 }
